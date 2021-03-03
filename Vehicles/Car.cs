9e295ca5471e447779e5f3e8fd62ae94b9ba35f6
Adesiji Olutunde <adesiji007@yahoo.com>
2021-03-03 03:01:12 +0000

    First commit

---
+++ Vehicles/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public partial class Car
+    {
+        //A partial method declaration consists of two parts: the definition and the implementation
+        //A partial Method definition
+        partial void InitializeCar();
+
+        // car Exterior Functionality
+        public void BuildTrim() { }
+        public void BuildWheels() { }
+    }
+
+    public partial class Car
+    {
+        //car Interior Functionality
+        public void BuildSeats() { }
+        public void BuildDashboard() { }
+    }
+
+    public partial class Car
+    {
+
+        // car Engine
+        public void BuildEngine() { }
+
+        // A partial method implementation
+        partial void InitializeCar()
+        {
+            string str = "car";
+            //put all car initialization here
+            throw new NotImplementedException();
+        }
+
+    }
+}
